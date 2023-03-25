0a94ba656bc3c8838c14b8e96c9d9d70afcd813b
melisand <melisandoval@live.com.ar>
2023-03-25 23:02:50 +0100

    create empty project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Data App</h1>
+  </header>
+  <main>
+    <p>Data</p>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div>
+      <p>
+        Favicon from
+        <a
+          href="https://www.flaticon.com/free-icons/growth"
+          title="growth icons"
+          >Growth icons created by Freepik - Flaticon</a
+        >
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
