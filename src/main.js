ed3fda81d7cc2275f4b1e4a717cba0f16c256efc
melisand <melisandoval@live.com.ar>
2023-03-26 16:50:41 +0200

    update store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { store } from "./store/data";
+import { store } from "./store/modules/data";
 
 const app = createApp(App);
 app.use(store);
